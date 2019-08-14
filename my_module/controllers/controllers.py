447b7d1c2ec89caa829c97426a354da611ae2ddd
TitoFlixa <48025076+TitoFlixa@users.noreply.github.com>
2019-08-14 16:48:15 +0000

    ang try 1

--- my_module/controllers/controllers.py
+++ my_module/controllers/controllers.py
@@ -1,21 +1,9 @@
-# -*- coding: utf-8 -*-
 from odoo import http
 
 
- class MyModule(http.Controller):
-     @http.route('/my_module', auth='public', website=True)
-     def index(self, **kw):
-         return http.request.render('my_module.index', {})
 
-#     @http.route('/my_module/my_module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_module.listing', {
-#             'root': '/my_module/my_module',
-#             'objects': http.request.env['my_module.my_module'].search([]),
-#         })
+class MyModule(http.Controller):
 
-#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('my_module.object', {
-#             'object': obj
-#         })
+    @http.route('/my_module', auth='public', website=True)
+    def index(self, **kw):
+        return http.request.render('my_module.index', {})
